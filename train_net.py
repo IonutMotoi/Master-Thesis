ac792691cca10a56151d784ded9afeeb0a62dd70
Ionut Motoi <ionut.m.motoi@gmail.com>
2021-07-24 16:49:26 +0200

    Update train_net.py

--- train_net.py
+++ train_net.py
@@ -9,3 +9,13 @@ from detectron2.utils.logger import setup_logger
 setup_logger()
 
 from detectron2.structures import BoxMode
+from detectron2.engine import DefaultTrainer, default_argument_parser, default_setup
+
+
+def main(args):
+    print("Hello")
+
+if __name__ == "__main__":
+    args = default_argument_parser().parse_args()
+    print("Command Line Args:", args)
+    main(args)
