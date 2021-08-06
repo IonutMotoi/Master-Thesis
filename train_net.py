b2c01c1a9910a3e0af5c10f5822e1813191660f1
Ionut Motoi <ionut.m.motoi@gmail.com>
2021-08-06 12:22:21 +0200

    basic train_net.py done (similar to plain_train_net.py)

--- train_net.py
+++ train_net.py
@@ -1,15 +1,18 @@
 import logging
 import os
 from collections import OrderedDict
-
+import torch
 from torch.nn.parallel import DistributedDataParallel
 
 from detectron2.checkpoint import DetectionCheckpointer
 from detectron2.config import get_cfg
-from detectron2.evaluation import COCOEvaluator
-from detectron2.engine import launch, default_argument_parser, default_setup
+from detectron2.data import build_detection_test_loader, build_detection_train_loader
+from detectron2.engine import launch, default_argument_parser, default_setup, PeriodicCheckpointer, default_writers
+from detectron2.evaluation import COCOEvaluator, inference_on_dataset, print_csv_format
 from detectron2.modeling import build_model
+from detectron2.solver import build_optimizer, build_lr_scheduler
 from detectron2.utils import comm
+from detectron2.utils.events import EventStorage
 
 logger = logging.getLogger("detectron2")
 
@@ -22,10 +25,65 @@ def get_evaluator(cfg, dataset_name, output_folder=None):
 
 def do_test(cfg, model):
     results = OrderedDict()
+    for dataset_name in cfg.DATASETS.TEST:
+        data_loader = build_detection_test_loader(cfg, dataset_name)
+        evaluator = get_evaluator(
+            cfg, dataset_name, os.path.join(cfg.OUTPUT_DIR, "inference", dataset_name)
+        )
+        results_i = inference_on_dataset(model, data_loader, evaluator)
+        results[dataset_name] = results_i
+        if comm.is_main_process():
+            logger.info("Evaluation results for {} in csv format:".format(dataset_name))
+            print_csv_format(results_i)
+    if len(results) == 1:
+        results = list(results.values())[0]
+    return results
 
 
 def do_train(cfg, model, resume=False):
-    pass
+    model.train()
+    optimizer = build_optimizer(cfg, model)
+    scheduler = build_lr_scheduler(cfg, model)
+    checkpointer = DetectionCheckpointer(model, cfg.OUTPUT_DIR, optimizer=optimizer, scheduler=scheduler)
+    start_iter = (checkpointer.resume_or_load(cfg.MODEL.WEIGHTS, resume=resume).get("iteration", -1) + 1)
+    max_iter = cfg.SOLVER.MAX_ITER
+    periodic_checkpointer = PeriodicCheckpointer(model, cfg.SOLVER.CHECKPOINT_PERIOD, max_iter=max_iter)
+    writers = default_writers(cfg.OUTPUT_DIR, max_iter) if comm.is_main_process() else []
+    data_loader = build_detection_train_loader(cfg)
+
+    logger.info("Starting training from iteration {}".format(start_iter))
+    with EventStorage(start_iter) as storage:
+        for data, iteration in zip(data_loader, range(start_iter, max_iter)):
+            storage.iter = iteration
+
+            loss_dict = model(data)
+            losses = sum(loss_dict.values())
+            assert torch.isfinite(losses).all(), loss_dict
+
+            loss_dict_reduced = {k: v.item() for k, v in comm.reduce_dict(loss_dict).items()}
+            losses_reduced = sum(loss for loss in loss_dict_reduced.values())
+            if comm.is_main_process():
+                storage.put_scalars(total_loss=losses_reduced, **loss_dict_reduced)
+
+            optimizer.zero_grad()
+            losses.backward()
+            optimizer.step()
+            storage.put_scalar("lr", optimizer.param_groups[0]["lr"], smoothing_hint=False)
+            scheduler.step()
+
+            if(
+                cfg.TEST.EVAL_PERIOD > 0
+                and (iteration + 1) % cfg.TEST.EVAL_PERIOD == 0
+                and iteration != max_iter - 1
+            ):
+                do_test(cfg, model)
+                comm.synchronize()
+
+            if iteration - start_iter > 5 and ((iteration + 1) % 20 == 0 or iteration == max_iter - 1):
+                for writer in writers:
+                    writer.write()
+
+            periodic_checkpointer.step(iteration)
 
 
 def setup():
