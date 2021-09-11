2556e62221408be81ae2d4a8f3ee40a4aeb1ef0b
Ionut Motoi <ionut.m.motoi@gmail.com>
2021-09-11 13:22:05 +0200

    Pixel dropout lambda function DONE

--- albumentations_mapper.py
+++ albumentations_mapper.py
@@ -206,12 +206,10 @@ def get_augmentations(cfg, is_train):
         augmentations.append(A.RandomBrightnessContrast())
 
     # Pixel Dropout
-    p = cfg.ALBUMENTATIONS.PIXEL_DROPOUT.DROPOUT_PROBABILITY
-    print(type(p))
     if cfg.ALBUMENTATIONS.PIXEL_DROPOUT.ENABLED:
         augmentations.append(A.Lambda(
             name="pixel_dropout",
-            image=lambda image, **kwargs: pixel_dropout(image, p=p),
+            image=lambda image, **kwargs: pixel_dropout(image, p=cfg.ALBUMENTATIONS.PIXEL_DROPOUT.DROPOUT_PROBABILITY),
             p=0.5))
 
     return augmentations
