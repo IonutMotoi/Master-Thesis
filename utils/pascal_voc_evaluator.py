1570d6e898c63b12cb0648cf0fb64bb4277cba2e
Ionut Motoi <ionut.m.motoi@gmail.com>
2021-10-08 12:26:43 +0200

    pascal_voc_evaluator.py Work in progress

--- utils/pascal_voc_evaluator.py
+++ utils/pascal_voc_evaluator.py
@@ -7,7 +7,7 @@ import logging
 import numpy as np
 import os
 import pickle
-from collections import OrderedDict
+from collections import OrderedDict, defaultdict
 import pycocotools.mask as mask_util
 import torch
 from pycocotools.coco import COCO
@@ -27,58 +27,82 @@ from detectron2.evaluation.evaluator import DatasetEvaluator
 
 
 class PascalVOCEvaluator(DatasetEvaluator):
-    def __init__(self):
-        self._cpu_device = torch.device("cpu")
-        self._predictions = []
-        self._results = OrderedDict()
+    """
+    Evaluate Pascal VOC 2007 style AP for detections and instance segmentation on a custom dataset.
+    """
+
+    def __init__(self, dataset_name):
+        meta = MetadataCatalog.get(dataset_name)
+        self.class_names = meta.thing_classes
+        self.cpu_device = torch.device("cpu")
+        self.results = OrderedDict()
+        self.predictions = None  # initialized inside reset()
+        self.annotations = None
 
     def reset(self):
-        self._predictions = []
+        self.predictions = defaultdict(list)  # class name -> list of prediction strings
+        self.annotations = {}  # image id -> dict with annotations (ground truth)
 
     def process(self, inputs, outputs):
-        for input, output in zip(inputs, outputs):
-            prediction = {
-                "image_id": input["image_id"],
-                "instances": []
-            }
+        for input_, output in zip(inputs, outputs):
+            image_id = input_["image_id"]
             instances = output["instances"].to(self._cpu_device)
+            boxes = instances.pred_boxes.tensor.numpy()
+            scores = instances.scores.tolist()
+            classes = instances.pred_classes.tolist()
 
-            num_instance = len(instances)
-            if num_instance != 0:
-                boxes = instances.pred_boxes.tensor.numpy()
-                scores = instances.scores.tolist()
-                classes = instances.pred_classes.tolist()
+            prediction = {
 
-                for k in range(num_instance):
-                    instance = {
-                        "image_id": input["image_id"],
-                        "category_id": classes[k],
-                        "bbox": boxes[k],
-                        "score": scores[k]
-                    }
-                    if instances.has("pred_masks"):
-                        instance["segmentation"] = instances.pred_masks[k]
-                    prediction["instances"].append(instance)
+            }
+            # why not just append all predictions in a list self.predictions?
 
-            self._predictions.append(prediction)
+            self.annotations[image_id] = input_["annotations"]
+            print(self.annotations)
+
+
+            # prediction = {
+            #     "image_id": input["image_id"],
+            #     "instances": []
+            # }
+            # instances = output["instances"].to(self._cpu_device)
+            #
+            # num_instance = len(instances)
+            # if num_instance != 0:
+            #     boxes = instances.pred_boxes.tensor.numpy()
+            #     scores = instances.scores.tolist()
+            #     classes = instances.pred_classes.tolist()
+            #
+            #     for k in range(num_instance):
+            #         instance = {
+            #             "image_id": input["image_id"],
+            #             "category_id": classes[k],
+            #             "bbox": boxes[k],
+            #             "score": scores[k]
+            #         }
+            #         if instances.has("pred_masks"):
+            #             instance["segmentation"] = instances.pred_masks[k]
+            #         prediction["instances"].append(instance)
+            #
+            # self._predictions.append(prediction)
 
     def evaluate(self):
-        predictions = self._predictions
-        self._results = OrderedDict()
+        # predictions = self._predictions
+        # self._results = OrderedDict()
+        #
+        # all_instances = list(itertools.chain(*[prediction["instances"] for prediction in predictions]))
+        #
+        # # Get tasks from predictions
+        # tasks = ["bbox"]
+        # if "segmentation" in all_instances[0]:
+        #     tasks.append("segm")
+        #
+        # for task in tasks:
+        #     self._results[task] = _evaluate_predictions(all_instances)
+        #
+        # # Copy so the caller can do whatever with results
+        # return copy.deepcopy(self._results)
+        pass
 
-        all_instances = list(itertools.chain(*[prediction["instances"] for prediction in predictions]))
 
-        # Get tasks from predictions
-        tasks = ["bbox"]
-        if "segmentation" in all_instances[0]:
-            tasks.append("segm")
-
-        for task in tasks:
-            self._results[task] = _evaluate_predictions(all_instances)
-
-        # Copy so the caller can do whatever with results
-        return copy.deepcopy(self._results)
-
-
-def _evaluate_predictions(all_instances):
-    return []
+def mean_average_precision(pred_boxes, pred_classes, pred_scores, true_boxes, iou_threshold=0.5):
+    average_precisions = []
