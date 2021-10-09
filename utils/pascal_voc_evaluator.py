9e35ac3b2277608ef2e2f2f917686ca6011987e1
Ionut Motoi <ionut.m.motoi@gmail.com>
2021-10-09 13:00:47 +0200

    optimized imports

--- utils/pascal_voc_evaluator.py
+++ utils/pascal_voc_evaluator.py
@@ -1,29 +1,8 @@
-import contextlib
-import copy
-import io
-import itertools
-import json
-import logging
-import numpy as np
-import os
-import pickle
 from collections import OrderedDict, defaultdict
-import pycocotools.mask as mask_util
+
+import numpy as np
 import torch
-from pycocotools.coco import COCO
-from pycocotools.cocoeval import COCOeval
-from tabulate import tabulate
-import sys
-
-import detectron2.utils.comm as comm
-from detectron2.config import CfgNode
 from detectron2.data import MetadataCatalog
-from detectron2.data.datasets.coco import convert_to_coco_json
-from detectron2.evaluation.fast_eval_api import COCOeval_opt
-from detectron2.structures import Boxes, BoxMode, pairwise_iou
-from detectron2.utils.file_io import PathManager
-from detectron2.utils.logger import create_small_table
-
 from detectron2.evaluation.evaluator import DatasetEvaluator
 
 
