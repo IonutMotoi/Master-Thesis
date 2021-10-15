60d1a64a578ee60b3abc2516cd2a740f1ff8ba1d
Ionut Motoi <ionut.m.motoi@gmail.com>
2021-10-15 15:37:42 +0200

    Removed video-input and refactored code

--- visualization_to_file.py
+++ visualization_to_file.py
@@ -1,20 +1,15 @@
 import argparse
 import glob
-import os
-import tempfile
-import time
-import warnings
-
-import cv2
 import multiprocessing
-import numpy as np
-import tqdm
+import os
+import time
 
+import tqdm
 from detectron2.config import get_cfg
 from detectron2.data.detection_utils import read_image
 from detectron2.utils.logger import setup_logger
 
-from utils.visualization_demo import VisualizationDemo
+from utils.visualization import Visualization
 
 
 def setup(args):
@@ -39,12 +34,12 @@ def get_parser():
         metavar="FILE",
         help="path to config file",
     )
-    parser.add_argument("--video-input", help="Path to video file.")
     parser.add_argument(
         "--input",
         nargs="+",
         help="A list of space separated input images; "
         "or a single glob pattern such as 'directory/*.jpg'",
+        required=True
     )
     parser.add_argument(
         "--output",
@@ -66,23 +61,6 @@ def get_parser():
     return parser
 
 
-def test_opencv_video_format(codec, file_ext):
-    with tempfile.TemporaryDirectory(prefix="video_format_test") as dir:
-        filename = os.path.join(dir, "test_file" + file_ext)
-        writer = cv2.VideoWriter(
-            filename=filename,
-            fourcc=cv2.VideoWriter_fourcc(*codec),
-            fps=float(30),
-            frameSize=(10, 10),
-            isColor=True,
-        )
-        [writer.write(np.zeros((10, 10, 3), np.uint8)) for _ in range(30)]
-        writer.release()
-        if os.path.isfile(filename):
-            return True
-        return False
-
-
 if __name__ == "__main__":
     multiprocessing.set_start_method("spawn", force=True)
     args = get_parser().parse_args()
@@ -92,65 +70,30 @@ if __name__ == "__main__":
 
     cfg = setup(args)
 
-    visualization_demo = VisualizationDemo(cfg)
+    visualization_demo = Visualization(cfg)
 
-    if args.input:
-        if len(args.input) == 1:
-            args.input = glob.glob(os.path.expanduser(args.input[0]))
-            assert args.input, "The input path(s) was not found"
-        for path in tqdm.tqdm(args.input, disable=not args.output):
-            # use PIL, to be consistent with evaluation
-            img = read_image(path, format="BGR")
-            start_time = time.time()
-            predictions, visualized_output = visualization_demo.run_on_image(img)
-            logger.info(
-                "{}: {} in {:.2f}s".format(
-                    path,
-                    "detected {} instances".format(len(predictions["instances"]))
-                    if "instances" in predictions
-                    else "finished",
-                    time.time() - start_time,
-                )
+    if len(args.input) == 1:
+        args.input = glob.glob(os.path.expanduser(args.input[0]))
+        assert args.input, "The input path(s) was not found"
+    for path in tqdm.tqdm(args.input, disable=not args.output):
+        # use PIL, to be consistent with evaluation
+        img = read_image(path, format="BGR")
+        start_time = time.time()
+        predictions, visualized_output = visualization_demo.run_on_image(img)
+        logger.info(
+            "{}: {} in {:.2f}s".format(
+                path,
+                "detected {} instances".format(len(predictions["instances"]))
+                if "instances" in predictions
+                else "finished",
+                time.time() - start_time,
             )
-
-            if os.path.isdir(args.output):
-                assert os.path.isdir(args.output), args.output
-                out_filename = os.path.join(args.output, os.path.basename(path))
-            else:
-                assert len(args.input) == 1, "Please specify a directory with args.output"
-                out_filename = args.output
-            visualized_output.save(out_filename)
-
-    elif args.video_input:
-        video = cv2.VideoCapture(args.video_input)
-        width = int(video.get(cv2.CAP_PROP_FRAME_WIDTH))
-        height = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
-        frames_per_second = video.get(cv2.CAP_PROP_FPS)
-        num_frames = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
-        basename = os.path.basename(args.video_input)
-        codec, file_ext = (
-            ("x264", ".mkv") if test_opencv_video_format("x264", ".mkv") else ("mp4v", ".mp4")
         )
-        if codec == ".mp4v":
-            warnings.warn("x264 codec not available, switching to mp4v")
 
         if os.path.isdir(args.output):
-            output_fname = os.path.join(args.output, basename)
-            output_fname = os.path.splitext(output_fname)[0] + file_ext
+            assert os.path.isdir(args.output), args.output
+            out_filename = os.path.join(args.output, os.path.basename(path))
         else:
-            output_fname = args.output
-        assert not os.path.isfile(output_fname), output_fname
-        output_file = cv2.VideoWriter(
-            filename=output_fname,
-            # some installation of opencv may not support x264 (due to its license),
-            # you can try other format (e.g. MPEG)
-            fourcc=cv2.VideoWriter_fourcc(*codec),
-            fps=float(frames_per_second),
-            frameSize=(width, height),
-            isColor=True,
-        )
-        assert os.path.isfile(args.video_input)
-        for vis_frame in tqdm.tqdm(visualization_demo.run_on_video(video), total=num_frames):
-            output_file.write(vis_frame)
-        video.release()
-        output_file.release()
+            assert len(args.input) == 1, "Please specify a directory with args.output"
+            out_filename = args.output
+        visualized_output.save(out_filename)
