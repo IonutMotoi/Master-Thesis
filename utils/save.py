f4a597ccb4519a7e9a434feaea96f687f8dbe331
Ionut Motoi <ionut.m.motoi@gmail.com>
2021-10-16 13:07:09 +0200

    Now it saves images in correct format

--- utils/save.py
+++ utils/save.py
@@ -5,13 +5,16 @@ import cv2
 import numpy as np
 
 
-def save_image_and_labels(dest_folder, img_id, image, class_labels, bboxes, masks):
+def save_image_and_labels(dest_folder, img_id, image, class_labels, bboxes, masks, img_format="RGB"):
     # Create destination folder
     Path(dest_folder).mkdir(parents=True, exist_ok=True)
 
     # Save image
     image_path = os.path.join(dest_folder, f'{img_id}.jpg')
-    cv2.imwrite(image_path, cv2.cvtColor(image, cv2.COLOR_RGB2BGR))
+    assert img_format in ["RGB", "BGR"]
+    if img_format == "RGB":
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+    cv2.imwrite(image_path, image)
 
     # Add object classes to bounding boxes
     bboxes = np.hstack((np.array(class_labels)[:, None], bboxes))
