f79c2b988ed4f17aca1fd29942319159f8af1a2a
Ionut Motoi <ionut.m.motoi@gmail.com>
2021-10-16 09:45:09 +0200

    refactored code

--- visualization_to_file.py
+++ visualization_to_file.py
@@ -1,63 +1,13 @@
-import argparse
 import glob
 import os
 import time
 
 import tqdm
-from detectron2.config import get_cfg
 from detectron2.data.detection_utils import read_image
 from detectron2.utils.logger import setup_logger
 
 from utils.visualization import Visualization
-
-
-def setup(args):
-    """
-    Create configs and perform basic setups.
-    """
-    cfg = get_cfg()
-    cfg.set_new_allowed(True)  # to allow merging new keys
-    cfg.merge_from_file(args.config_file)
-    cfg.merge_from_list(args.opts)
-    # Set score_threshold
-    cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = args.confidence_threshold
-    cfg.freeze()
-    return cfg
-
-
-def get_parser():
-    parser = argparse.ArgumentParser(description="Detectron2 inference")
-    parser.add_argument(
-        "--config-file",
-        default="./configs/inference.yaml",
-        metavar="FILE",
-        help="path to config file",
-    )
-    parser.add_argument(
-        "--input",
-        nargs="+",
-        help="A list of space separated input images; "
-        "or a single glob pattern such as 'directory/*.jpg'",
-        required=True
-    )
-    parser.add_argument(
-        "--output",
-        help="A file or directory to save output visualizations. ",
-        required=True
-    )
-    parser.add_argument(
-        "--confidence-threshold",
-        type=float,
-        default=0.5,
-        help="Minimum score for instance predictions to be shown",
-    )
-    parser.add_argument(
-        "--opts",
-        help="Modify config options using the command-line 'KEY VALUE' pairs",
-        default=[],
-        nargs=argparse.REMAINDER,
-    )
-    return parser
+from utils.inference_setup import setup, get_parser
 
 
 if __name__ == "__main__":
