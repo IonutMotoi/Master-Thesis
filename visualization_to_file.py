2dcd705330d6e11b579da7b3bfc7d3cf877a8053
Ionut Motoi <ionut.m.motoi@gmail.com>
2021-10-16 09:39:20 +0200

    removed multiprocessing.set_start_method()

--- visualization_to_file.py
+++ visualization_to_file.py
@@ -62,7 +62,6 @@ def get_parser():
 
 
 if __name__ == "__main__":
-    multiprocessing.set_start_method("spawn", force=True)
     args = get_parser().parse_args()
     setup_logger(name="fvcore")
     logger = setup_logger()
