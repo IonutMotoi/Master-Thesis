7cfc236ad328aecf75b886e166556b548f00dc2a
Ionut Motoi <ionut.m.motoi@gmail.com>
2021-11-03 17:35:16 +0100

    WIP Albumentations mapper with pad and scale with detectron2 tools

--- utils/albumentations_mapper.py
+++ utils/albumentations_mapper.py
@@ -5,6 +5,8 @@ import torch
 from pycocotools.mask import encode
 import cv2
 import albumentations as A
+
+import detectron2.data.transforms as T
 from detectron2.data import detection_utils
 
 from utils.bbox_conversion import pascal_voc_bboxes_to_albumentations, albumentations_bboxes_to_pascal_voc
@@ -21,8 +23,9 @@ class AlbumentationsMapper:
 
     The callable does the following:
     1. Read the image from "file_name"
-    2. Apply augmentations/transforms to the image and annotations with Albumentations
-    3. Prepare data and annotations to Tensor and :class:`Instances`
+    2. Use detectron tools to resize and crop the image (ResizeShortestEdge and RandomCrop)
+    3. Apply augmentations/transforms to the image and annotations with Albumentations
+    4. Prepare data and annotations and convert them to Tensor and :class:`Instances`
     """
 
     def __init__(self, cfg, is_train: bool = True, is_valid: bool = False):
@@ -34,17 +37,21 @@ class AlbumentationsMapper:
         """
         self.is_train = is_train
         self.is_valid = is_valid
-        self.use_instance_mask = cfg.MODEL.MASK_ON
         self.instance_mask_format = cfg.INPUT.MASK_FORMAT
+        self.resize_gen = detection_utils.build_transform_gen(cfg, is_train)
+        if cfg.INPUT.CROP.ENABLED and is_train:
+            self.crop_gen = T.RandomCrop(cfg.INPUT.CROP.TYPE, cfg.INPUT.CROP.SIZE)
+        else:
+            self.crop_gen = None
 
         # Define augmentations
         augmentations = get_augmentations(cfg, is_train)
         if is_train:
-            self.transform = A.Compose(
+            self.augmentations = A.Compose(
                 augmentations,
                 bbox_params=A.BboxParams(format='albumentations', label_fields=['class_labels', 'bbox_ids']))
         else:
-            self.transform = None
+            self.augmentations = None
 
         # Log
         logger = logging.getLogger("detectron2")
@@ -94,7 +101,7 @@ class AlbumentationsMapper:
         bboxes = pascal_voc_bboxes_to_albumentations(bboxes, height=image.shape[0], width=image.shape[1])
 
         # Apply transformations
-        transformed = self.transform(
+        transformed = self.augmentations(
             image=image,
             bboxes=bboxes,
             masks=masks,
@@ -129,20 +136,38 @@ class AlbumentationsMapper:
             for i in range(len(bboxes))
         ]
 
-        image_shape = image.shape[:2]  # H, W
-        dataset_dict["height"] = image_shape[0]
-        dataset_dict["width"] = image_shape[1]
+        if self.crop_gen:
+            # image crop using detectron2 tools
+            crop_tfm = detection_utils.gen_crop_transform_with_instance(
+                self.crop_gen.get_crop_size(image.shape[:2]),
+                image.shape[:2],
+                np.random.choice(dataset_dict["annotations"])
+            )
+            image = crop_tfm.apply_image(image)
+
+        # image resize using detectron2 tools
+        image, transforms = T.apply_transform_gens(self.resize_gen, image)
+        if self.crop_gen:
+            transforms = crop_tfm + transforms
+
+        annos = [
+            detection_utils.transform_instance_annotations(obj, transforms, image.shape[:2])
+            for obj in dataset_dict.pop("annotations")
+            if obj.get("iscrowd", 0) == 0
+        ]
+        dataset_dict['annotations'] = annos
+
+        instances = detection_utils.annotations_to_instances(
+            annos, image.shape[:2], mask_format=self.instance_mask_format
+        )
+        dataset_dict["instances"] = detection_utils.filter_empty_instances(instances)
+
+        dataset_dict["height"] = image.shape[0]
+        dataset_dict["width"] = image.shape[1]
         # RGB to BGR required by the model
         image = image[:, :, ::-1]
         # Convert H,W,C image to C,H,W tensor
         dataset_dict["image"] = torch.as_tensor(np.ascontiguousarray(image.transpose((2, 0, 1))))
-
-        annos = [anno for anno in dataset_dict.pop("annotations") if anno.get("iscrowd", 0) == 0]
-
-        instances = detection_utils.annotations_to_instances(
-            annos, image_shape, mask_format=self.instance_mask_format
-        )
-        dataset_dict["instances"] = detection_utils.filter_empty_instances(instances)
 
         return dataset_dict
 
@@ -180,27 +205,8 @@ def pixel_dropout(image, p, **kwargs):
 def get_augmentations(cfg, is_train):
     augmentations = []
 
-    # Longest Max Size
-    if cfg.ALBUMENTATIONS.LONGEST_MAX_SIZE.ENABLED:
-        augmentations.append(A.LongestMaxSize(max_size=cfg.ALBUMENTATIONS.LONGEST_MAX_SIZE.VALUE))
-    # Pad
-    if cfg.ALBUMENTATIONS.PAD.ENABLED:
-        augmentations.append(A.PadIfNeeded(
-            min_height=cfg.ALBUMENTATIONS.PAD.TARGET_HEIGHT,
-            min_width=cfg.ALBUMENTATIONS.PAD.TARGET_WIDTH,
-            border_mode=cv2.BORDER_CONSTANT,
-            value=cfg.ALBUMENTATIONS.PAD.VALUE,
-            mask_value=cfg.ALBUMENTATIONS.PAD.MASK_VALUE
-        ))
-
-    # Only Longest Max Size and Pad during evaluation
-    # Those transformations were already applied, here are returned for print purposes
     if not is_train:
         return augmentations
-
-    # Horizontal Flip
-    if cfg.ALBUMENTATIONS.HORIZONTAL_FLIP.ENABLED:
-        augmentations.append(A.HorizontalFlip())
 
     # Pixel Dropout
     if cfg.ALBUMENTATIONS.PIXEL_DROPOUT.ENABLED:
