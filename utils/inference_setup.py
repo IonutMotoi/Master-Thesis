2b709b4f565faa637fc72b73cc4785add653a39f
Ionut Motoi <ionut.m.motoi@gmail.com>
2021-11-05 19:24:34 +0100

    modified help description

--- utils/inference_setup.py
+++ utils/inference_setup.py
@@ -34,7 +34,7 @@ def get_parser():
     )
     parser.add_argument(
         "--output",
-        help="Directory to save output visualizations",
+        help="Directory to save output",
         required=True
     )
     parser.add_argument(
