4afd781eb494676e29a40ded33df8f3ebc5336f3
Ionut Motoi <ionut.m.motoi@gmail.com>
2021-11-08 15:25:47 +0100

    Removed required from arguments

--- get_masks_from_bboxes.py
+++ get_masks_from_bboxes.py
@@ -10,15 +10,12 @@ from pseudo_labeling.masks_from_bboxes import MasksFromBboxes
 if __name__ == "__main__":
     parser = get_parser()
     parser.add_argument("--ids",
-                        required=True,
                         default="/thesis/wgisd/train_without_masked_train_and_valid_ids.txt",
                         help="Also save an image with labels overlay")
     parser.add_argument("--data",
-                        required=True,
                         default="/thesis/wgisd/data",
                         help="Also save an image with labels overlay")
     parser.add_argument("--dest",
-                        required=True,
                         default="./pseudo_labels",
                         help="Also save an image with labels overlay")
     args = parser.parse_args()
