0e98e35ab7edcb9471533dcc6cdf8245be2c1def
Ionut Motoi <ionut.m.motoi@gmail.com>
2021-11-08 10:17:45 +0100

    Added documentation

--- utils/predictor.py
+++ utils/predictor.py
@@ -9,30 +9,15 @@ from detectron2.structures import Instances, Boxes
 
 class MasksFromBboxesPredictor:
     """
-    Create a simple end-to-end predictor with the given config that runs on
-    single device for a single input image.
+    Create a simple end-to-end mask predictor with the given config that runs on
+    single device for a single input image with GT (Ground-Truth) bounding boxes and classes.
 
     Compared to using the model directly, this class does the following additions:
-
     1. Load checkpoint from `cfg.MODEL.WEIGHTS`.
     2. Always take BGR image as the input and apply conversion defined by `cfg.INPUT.FORMAT`.
-    3. Apply resizing defined by `cfg.INPUT.{MIN,MAX}_SIZE_TEST`.
-    4. Take one input image and produce a single output, instead of a batch.
-
-    This is meant for simple demo purposes, so it does the above steps automatically.
-    This is not meant for benchmarks or running complicated inference logic.
-    If you'd like to do anything more complicated, please refer to its source code as
-    examples to build and use the model manually.
-
-    Attributes:
-        metadata (Metadata): the metadata of the underlying dataset, obtained from
-            cfg.DATASETS.TEST.
-
-    Examples:
-    ::
-        pred = DefaultPredictor(cfg)
-        inputs = cv2.imread("input.jpg")
-        outputs = pred(inputs)
+       As input it also requires the GT bboxes and classes that will be used to predict the masks.
+    3. Apply resizing defined by `cfg.INPUT.{MIN,MAX}_SIZE_TEST` to both image and labels
+    4. Take one input image with the bounding boxes and classes and produce a single output, instead of a batch.
     """
 
     def __init__(self, cfg):
@@ -56,10 +41,8 @@ class MasksFromBboxesPredictor:
         """
         Args:
             original_image (np.ndarray): an image of shape (H, W, C) (in BGR order).
-            target (Instances): The `Instances` object contains "pred_boxes" and
-                                "pred_classes" which are known boxes in the image.
-                                The inference will then skip the detection of bounding
-                                boxes, and only predict other per-ROI outputs.
+            bboxes (list): List of bounding boxes (Nx4) in the Pascal VOC format.
+            classes (list): List of categories for each bounding box
         Returns:
             predictions (dict):
                 the output of the model for one image only.
@@ -73,13 +56,15 @@ class MasksFromBboxesPredictor:
             height, width = original_image.shape[:2]
             transform = self.aug.get_transform(original_image)
 
+            # Transform the image
             image = transform.apply_image(original_image)
             image = torch.as_tensor(image.astype("float32").transpose(2, 0, 1))  # C, H, W
 
+            # Transform the GT bounding boxes
             bboxes = [transform.apply_box(np.array([bbox]))[0].clip(min=0) for bbox in bboxes]
             bboxes = torch.tensor(bboxes)
 
-            # Create an 'Instances' object
+            # Create an 'Instances' object with the GT bboxes and classes
             target = Instances(image_size=image.shape[1:])
             target.pred_boxes = Boxes(bboxes)
             target.pred_classes = torch.tensor(classes, dtype=torch.int64)
