1e5cc9844e3f9d033cf48e167f80d8e55c81e243
Ionut Motoi <ionut.m.motoi@gmail.com>
2021-11-08 20:53:02 +0100

    Refactored code

--- train_net.py
+++ train_net.py
@@ -112,6 +112,13 @@ def do_train(cfg, model, resume=False):
 
                 comm.synchronize()
 
+            # # Visualize some examples of augmented images and annotations
+            # # There is a bug with the iteration count in the writer
+            # if examples_count < 10:
+            #     image = visualize_image_and_annotations(data[0])
+            #     storage.put_image("Example of augmented image", image)
+            #     examples_count += 1
+
             # Write events to EventStorage periodically
             if iteration - start_iter > 5 and (
                     (iteration + 1) % cfg.TEST.EVAL_PERIOD == 0 or iteration == max_iter - 1
