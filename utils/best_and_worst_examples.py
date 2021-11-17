0990f7069026ac3b8e0044243797f1b9d00886a5
Ionut Motoi <ionut.m.motoi@gmail.com>
2021-11-17 18:12:20 +0100

    specify step

--- utils/best_and_worst_examples.py
+++ utils/best_and_worst_examples.py
@@ -68,7 +68,7 @@ def log_selected_images(results, caption="", max_res=3):
                         }
                     },
                     caption=sample["image_id"])
-        wandb.log({caption: img})
+        wandb.log({caption: img}, step=i)
 
         if i == max_res-1:
             break
