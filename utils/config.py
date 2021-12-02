4a997fa9ad42f1814455e742b4c091b468f58ed7
Ionut Motoi <ionut.m.motoi@gmail.com>
2021-12-02 09:50:20 +0100

    Added config utils file

---
+++ utils/config.py
@@ -0,0 +1,16 @@
+def get_hyperparameters(cfg):
+    """
+    Get the hyperparameters that we want to pass to wandb in order to do a hyperparameter sweep
+    :param cfg: detectron2 cfg
+    :return: (dict) Hyperparameters
+    """
+    hyperparameters = dict(
+        test=1
+    )
+    return hyperparameters
+
+
+def set_config_from_hyperparameters(cfg, hyperparameters):
+    if cfg.is_frozen():
+        cfg.defrost()
+    pass
