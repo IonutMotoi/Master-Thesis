958f7babb4eda63a562d6d980610601a548fbde2
Ionut Motoi <ionut.m.motoi@gmail.com>
2021-12-11 13:07:05 +0100

    slic_zero sweep on different thresholds

--- sweep/sweep_utils.py
+++ sweep/sweep_utils.py
@@ -24,10 +24,12 @@ def set_config_from_sweep(cfg, sweep_params):
     cfg.SOLVER.MAX_TRAINING_ROUNDS = sweep_params.max_training_rounds
     cfg.MODEL.WEIGHTS = sweep_params.model_weights
     cfg.PSEUDOMASKS.SLIC.N_SEGMENTS = sweep_params.n_segments
-    if sweep_params.compactness == 'slic_zero':
-        cfg.PSEUDOMASKS.SLIC.SLIC_ZERO = sweep_params.slic_zero
-    else:
-        cfg.PSEUDOMASKS.SLIC.COMPACTNESS = sweep_params.compactness
+    # if sweep_params.compactness == 'slic_zero':
+    #     cfg.PSEUDOMASKS.SLIC.SLIC_ZERO = True
+    # else:
+    #     cfg.PSEUDOMASKS.SLIC.COMPACTNESS = sweep_params.compactness
+    cfg.PSEUDOMASKS.SLIC.SLIC_ZERO = sweep_params.slic_zero
+    cfg.PSEUDOMASKS.SLIC.COMPACTNESS = sweep_params.compactness
     cfg.PSEUDOMASKS.SLIC.SIGMA = sweep_params.sigma
     cfg.PSEUDOMASKS.SLIC.THRESHOLD = sweep_params.threshold
     if 'model_final_a3ec72.pkl' not in sweep_params.model_weights:
