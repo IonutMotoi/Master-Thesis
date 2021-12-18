a4ee563a03b23b214701c952061cebab87cbee1f
Ionut Motoi <ionut.m.motoi@gmail.com>
2021-12-18 21:16:44 +0100

    dilation demo

--- pseudo_labeling/dilation_demo.py
+++ pseudo_labeling/dilation_demo.py
@@ -31,11 +31,9 @@ def plot_bboxes_yolo(image, bboxes, ax):
 
 
 if __name__ == "__main__":
-    # img_id = "IMG_20210924_131131409"
-    # img_id = "IMG_20210924_112427127"
-    img_id = "IMG_20210924_131131409"
+    img_id = "IMG_20210924_160704843"
     data_path = "./new_dataset/train"
-    mask_path = "./pseudo_labels"
+    mask_path = "./pseudo_masks/new_dataset"
 
     image = cv2.imread(os.path.join(data_path, f'{img_id}.jpg'))
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
@@ -49,40 +47,34 @@ if __name__ == "__main__":
 
     masks = np.load(os.path.join(mask_path, f'{img_id}.npz'))['arr_0'].astype(np.uint8)
 
-    mask = masks[:, :, 1]
-    bbox = bboxes[1]
-
-    fig, (ax1, ax2) = plt.subplots(1, 2)
+    index = 0
+    mask = masks[:, :, index]
+    bbox = bboxes[index]
 
     # Before dilation
+    fig1, ax1 = plt.subplots()
     ax1.imshow(image)
     ax1.imshow(mask, alpha=0.5)
     plot_bboxes_yolo(image, [bbox], ax1)
-
-    kernel = get_default_kernel()
+    ax1.set_axis_off()
+    plt.savefig('0_grape_none.png', bbox_inches='tight', pad_inches=0)
 
     # Dilation
+    fig2, ax2 = plt.subplots()
     ax2.imshow(image)
-    obj = ax2.imshow(mask, alpha=0.5)
     plot_bboxes_yolo(image, [bbox], ax2)
     abs_bbox = utils.bbox_conversion.yolo_bbox_to_pascal_voc(bbox, img_height=height, img_width=width)
 
-    i = 0
-    while not np.all((mask == 0)) and not mask_touches_bbox(mask, abs_bbox, touches_all_edges=False):
-        mask = cv2.dilate(mask, kernel, iterations=1)
+    kernel = get_default_kernel()
+    dilated_mask = mask.copy()
+    if not np.all((dilated_mask == 0)):
+        while not mask_touches_bbox(dilated_mask, abs_bbox, touches_all_edges=False):
+            dilated_mask = cv2.dilate(dilated_mask, kernel, iterations=1)
+    set_values_outside_bbox_to_zero(dilated_mask, abs_bbox)
+    dilated_mask = (dilated_mask - mask)
 
-        i += 1
-        if i % 10 == 0:  # Update shown mask every 10 iterations
-            obj.set_data(mask)
-            plt.draw()
-            plt.pause(1e-2)
-    obj.set_data(mask)
-    plt.draw()
-    plt.pause(1)
-
-    set_values_outside_bbox_to_zero(mask, abs_bbox)
-
-    obj.set_data(mask)
-    plt.draw()
+    ax2.imshow(dilated_mask+mask*3, alpha=0.5)
+    ax2.set_axis_off()
+    plt.savefig('0_grape_dilation.png', bbox_inches='tight', pad_inches=0)
 
     plt.show()
