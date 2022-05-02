db4d6b85dd2745115817c9cb36eb9e31944595ad
Ionut Motoi <ionut.m.motoi@gmail.com>
2022-05-02 11:34:26 +0200

    new plots

--- demo/graphs/methods_iterative10.py
+++ demo/graphs/methods_iterative10.py
@@ -3,24 +3,28 @@ from matplotlib import pyplot as plt
 import statistics
 
 if __name__ == "__main__":
-    aps_none = [58.633, 58.215, 58.563, 57.456, 57.533, 56.657, 56.886, 56.927, 56.972, 57.364]
-    stds_none = [1.140, 1.498, 1.556, 2.236, 1.781, 1.552, 1.399, 1.514, 1.097, 1.734]
+    aps_none = [48.329,	48.141,	45.311,	41.942,	50.976,	42.951,	46.611, 43.21, 45.731]
+    # stds_none = [1.140, 1.498, 1.556, 2.236, 1.781, 1.552, 1.399, 1.514, 1.097, 1.734]
 
-    aps_dilation = [60.682, 60.311, 59.271, 57.531, 56.608, 56.875, 55.842, 55.924, 54.839, 54.460]
-    stds_dilation = [0.692, 1.081, 0.912, 0.446, 1.222, 1.090, 1.279, 1.321, 0.743, 0.872]
+    aps_dilation = [48.575,	53.28,	49.65,	50.251,	49.107,	48.948,	46.158,	47.092,	46.6]
+    # stds_dilation = [0.692, 1.081, 0.912, 0.446, 1.222, 1.090, 1.279, 1.321, 0.743, 0.872]
 
-    aps_slic = [58.721, 58.307, 57.470, 57.299, 55.964, 56.205, 55.666, 56.652, 55.845, 55.166]
-    stds_slic = [0.857, 0.851, 0.967, 0.573, 1.676, 2.577, 2.159, 2.201, 1.483, 1.833]
+    aps_slic = [50.527,	47.585,	47.438,	44.942,	45.713,	43.446,	45.335,	47.357,	47.103]
+    # stds_slic = [0.857, 0.851, 0.967, 0.573, 1.676, 2.577, 2.159, 2.201, 1.483, 1.833]
 
-    aps_grabcut = [61.057, 61.081, 60.871, 61.305, 61.412, 61.543, 61.095, 61.475, 61.374, 61.460]
-    stds_grabcut = [1.685, 0.716, 0.120, 1.400, 0.387, 0.455, 0.765, 0.907, 0.923, 0.652]
+    aps_grabcut = [49.819, 52.047, 53.255,	49.79,	51.4,	46.791,	49.773,	49.042,	47.676]
+    # stds_grabcut = [1.685, 0.716, 0.120, 1.400, 0.387, 0.455, 0.765, 0.907, 0.923, 0.652]
 
-    train_rounds = np.arange(start=1, stop=11)
+    train_rounds = np.arange(start=1, stop=10)
     fig, ax = plt.subplots()
-    ax.errorbar(train_rounds, aps_none, None, capsize=5, marker='d', label='Pseudo-masks')
-    ax.errorbar(train_rounds, aps_dilation, None, capsize=5, marker='d', label='Dilation method')
-    ax.errorbar(train_rounds, aps_slic, None, capsize=5, marker='d', label='Slic method')
-    ax.errorbar(train_rounds, aps_grabcut, None, capsize=5, marker='d', label='Grabcut method')
+    ax.plot(train_rounds, aps_none, label='Pseudo-masks')
+    ax.plot(train_rounds, aps_dilation, label='Dilation method')
+    ax.plot(train_rounds, aps_slic, label='Slic method')
+    ax.plot(train_rounds, aps_grabcut, label='Grabcut method')
+    # ax.errorbar(train_rounds, aps_none, None, capsize=5, marker='d', label='Pseudo-masks')
+    # ax.errorbar(train_rounds, aps_dilation, None, capsize=5, marker='d', label='Dilation method')
+    # ax.errorbar(train_rounds, aps_slic, None, capsize=5, marker='d', label='Slic method')
+    # ax.errorbar(train_rounds, aps_grabcut, None, capsize=5, marker='d', label='Grabcut method')
     ax.set_xticks(train_rounds)
     ax.set_ylabel('Segmentation Average Precision (AP)')
     ax.set_xlabel('Training rounds')
